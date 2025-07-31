1e6cdc8b4bf1826c12376554ca60229d95d92dff
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2025-07-31 10:59:15 +0300

    rspamd contrib

---
+++ contrib/rspamd/lua.local.d/rqwatch_mime_from_rcpt_to_blacklist.lua
@@ -0,0 +1,49 @@
+local logger = require "rspamd_logger"
+logger.infox("RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST loaded")
+
+local function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local from_to_map = rspamd_config:add_map{
+  type = "set",
+  description = "Blacklist MIME From|To combinations",
+  url = "http://127.0.0.1/maps/mime_from_rcpt_to_blacklist.txt"
+}
+
+rspamd_config:register_symbol{
+  name = "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST",
+  callback = function(task)
+    local from = task:get_from('mime') -- MIME only
+    local rcpt = task:get_recipients('mime') -- MIME only
+
+    if from and rcpt then
+      for _, f in ipairs(from) do
+        for _, r in ipairs(rcpt) do
+		    if f.addr and r.addr then
+            local key = string.format("%s|%s", trim(f.addr:lower()), trim(r.addr:lower()))
+            logger.infox(task, "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST: checking key: %s", key)
+
+            if from_to_map:get_key(key) then
+              logger.infox(task, "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST: Match for key: %s", key)
+              task:insert_result("RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST", 1.0, key)
+              return true
+            else
+              logger.infox(task, "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST: No match for key: %s", key)
+            end
+
+			 else
+            logger.infox(task, "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST: Missing from or to addr")
+		    end
+		  end
+      end
+    else
+      logger.infox(task, "RQWATCH_MIME_FROM_RCPT_TO_BLACKLIST: Missing from or to")
+    end
+
+    return false
+  end,
+  priority = 10,
+  type = "normal",
+  score = 1.0, -- Metric score
+}
---
+++ contrib/rspamd/lua.local.d/rqwatch_smtp_from_rcpt_to_whitelist.lua
@@ -0,0 +1,49 @@
+local logger = require "rspamd_logger"
+logger.infox("RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST loaded")
+
+local function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local from_to_map = rspamd_config:add_map{
+  type = "set",
+  description = "Whitelist SMTP From|To combinations",
+  url = "http://127.0.0.1/maps/mail_from_rcpt_to_whitelist.txt"
+}
+
+rspamd_config:register_symbol{
+  name = "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST",
+  callback = function(task)
+    local from = task:get_from('smtp') -- SMTP only
+    local rcpt = task:get_recipients('smtp') -- SMTP only
+
+    if from and rcpt then
+      for _, f in ipairs(from) do
+        for _, r in ipairs(rcpt) do
+		    if f.addr and r.addr then
+            local key = string.format("%s|%s", trim(f.addr:lower()), trim(r.addr:lower()))
+            logger.infox(task, "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST: checking key: %s", key)
+
+            if from_to_map:get_key(key) then
+              logger.infox(task, "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST: Match for key: %s", key)
+              task:insert_result("RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST", 1.0, key)
+              return true
+            else
+              logger.infox(task, "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST: No match for key: %s", key)
+            end
+
+			 else
+            logger.infox(task, "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST: Missing from or to addr")
+		    end
+		  end
+      end
+    else
+      logger.infox(task, "RQWATCH_SMTP_FROM_RCPT_TO_WHITELIST: Missing from or to")
+    end
+
+    return false
+  end,
+  priority = 10,
+  type = "normal",
+  score = 1.0, -- Metric score
+}
