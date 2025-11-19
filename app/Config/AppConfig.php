f374b0c2380dd538fe1c6049f73bfb8b8cd24052
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2025-11-19 14:18:04 +0200

    Add type in configs

--- app/Config/AppConfig.php
+++ app/Config/AppConfig.php
@@ -16,48 +16,48 @@ define('APP_ROOT', realpath(__DIR__ . '/../..'));
 class AppConfig {
 
 // Application version
-public const VERSION = '1.6.9-dev2';
+public const string VERSION = '1.6.9-dev2';
 
-public const APP_NAME = 'Rqwatch';
+public const string APP_NAME = 'Rqwatch';
 
 // Prefix to use in syslog
-public const SYSLOG_PREFIX = 'rqwatch';
+public const string SYSLOG_PREFIX = 'rqwatch';
 
-public const LOG_FILE = APP_ROOT . '/logs/rqwatch.log';
+public const string LOG_FILE = APP_ROOT . '/logs/rqwatch.log';
 
-public const APP_INFO = 'Rspamd Quarantine Watch';
+public const string APP_INFO = 'Rspamd Quarantine Watch';
 
-public const APP_LOGO_PATH = '/images/logo.png';
+public const string APP_LOGO_PATH = '/images/logo.png';
 
-public const APP_LOGO_ALT = self::APP_NAME . ' logo';
+public const string APP_LOGO_ALT = self::APP_NAME . ' logo';
 
 // Paths to configuration files
-public const CONFIG_DEFAULT_PATH = APP_ROOT . '/config/config.php';
-public const CONFIG_LOCAL_PATH   = APP_ROOT . '/config/config.local.php';
+public const string CONFIG_DEFAULT_PATH = APP_ROOT . '/config/config.php';
+public const string CONFIG_LOCAL_PATH   = APP_ROOT . '/config/config.local.php';
 
 // Composer autoload
-public const VENDOR_PATH = APP_ROOT . '/vendor/autoload.php';
+public const string VENDOR_PATH = APP_ROOT . '/vendor/autoload.php';
 
 // Environment file
-public const ENV_PATH = APP_ROOT . '/.env';
+public const string ENV_PATH = APP_ROOT . '/.env';
 
 // Views directory
-public const VIEWS_PATH = APP_ROOT . '/app/Views';
+public const string VIEWS_PATH = APP_ROOT . '/app/Views';
 
 // Directory for storing and serving map files
 
-public const MAP_DIR = APP_ROOT . '/web/maps/';
+public const string MAP_DIR = APP_ROOT . '/web/maps/';
 
-public const GITHUB = 'https://github.com/bilias/rqwatch/';
+public const string GITHUB = 'https://github.com/bilias/rqwatch/';
 
 // Default password hash for local users
 // https://www.php.net/manual/en/function.password-hash.php
-public const DEFAULT_PASSWORD_HASH = PASSWORD_BCRYPT;
+public const string DEFAULT_PASSWORD_HASH = PASSWORD_BCRYPT;
 
 // Path to use for remote API mail release
-public const RELEASE_MAIL_API_PATH = '/api/release_mail.php';
+public const string RELEASE_MAIL_API_PATH = '/api/release_mail.php';
 
 // Path to use for remote API get mail
-public const GET_MAIL_API_PATH = '/api/get_mail.php';
+public const string GET_MAIL_API_PATH = '/api/get_mail.php';
 
 }
