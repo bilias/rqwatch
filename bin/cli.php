e5f8e306775921ad67162475996f870a7b5eef49
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2025-11-19 16:40:31 +0200

    Add license in cli.php

--- bin/cli.php
+++ bin/cli.php
@@ -1,5 +1,14 @@
 #!/usr/bin/env php
 <?php
+/*
+ Rqwatch
+ Copyright (C) 2025 Giannis Kapetanakis
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 define('CLI_MODE', true);
 
 use App\Kernel;
