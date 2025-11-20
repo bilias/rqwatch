110511087cd9bbd58cb1b1242e4810c21fcd8727
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2025-11-20 11:02:57 +0200

    Usused variable

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -433,8 +433,9 @@ class UserController extends ViewController
 	}
 
 	public function getMailAliases(User $user): array {
-		$mail_aliases = [];
 		/*
+		$mail_aliases = [];
+
 		foreach ($user->mailAliases as $mail_alias) {
 			$mail_aliases[] = strtolower(trim($mail_alias->alias));
 		}
