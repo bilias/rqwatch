9113fdbf6166e53e84939e5b8d13189c106f7c51
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-07-07 14:14:51 +0300

    Use updateCreateUser() and getRedirectUrl in login()

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -38,10 +38,11 @@ class LoginController extends ViewController
 	}
 
 	public function login(): Response {
+		$this->initUrls();
+
 		if (!empty($this->session->get('username'))) {
 			$this->fileLogger->warning("'{$this->session->get('username')}' Already logged in");
 			$this->flashbag->add('info', "Already logged in");
-			$this->initUrls();
 			return new RedirectResponse($this->homepageUrl);
 		}
 
@@ -67,125 +68,40 @@ class LoginController extends ViewController
 			$data = $loginform->getData();
 			$username = strtolower(trim($data['username']));
 			$password = trim($loginform->get('password')->getData());
+
 			if (empty($username) or empty($password)) {
 				sleep((int)$_ENV['FAILED_LOGIN_TIMEOUT']);
 				$this->flashbag->add('error', 'Login credentials missing');
-				$this->initUrls();
 				return new RedirectResponse($this->loginUrl);
 			}
 
 			$auth = new AuthManager($this->fileLogger);
 
-			$db_user_not_found = false;
-			if ($auth->authenticate($username, $password)) {
-				$username = $auth->getAuthenticatedUser();
-				$is_admin = $auth->getIsAdmin();
-				$email = $auth->getUserEmail();
-				$auth_provider = $auth->getAuthProvider();
-				$user_id = $auth->getUserId();
-
-				// DB
-				if ($auth_provider === "DB") {
-					// save last login information
-					User::where('username', $username)
-							->update([
-								'last_login' => date("Y-m-d H:i:s"),
-								'updated_at' => DB::raw('updated_at'),
-							]);
-				// LDAP
-				} else {
-					$user = User::where('username', $username)->first();
-					// LDAP user does not exist, create him
-					if (!$user) {
-						$user = new User();
-						$user->username = $username;
-						$user->email = $email;
-						$user->firstname = $auth->getUserFirstName();
-						$user->lastname = $auth->getUserLastName();
-						$user->is_admin = $is_admin;
-						$user->auth_provider = $auth->getAuthProviderId();
-						$user->password = "EXTERNAL_AUTH";
-						$user->last_login = date("Y-m-d H:i:s");
-						$user->save();
-					// LDAP user already exists
-					} else {
-						$user->update([ 'last_login' => date("Y-m-d H:i:s") ]);
-					}
-					$user_id = $user->id;
-				}
-				$this->session->set('username', $username);
-				$this->session->set('email', $email);
-				$this->session->set('is_admin', $is_admin);
-				$this->session->set('auth_provider', $auth_provider);
-				$this->session->set('user_id', $user_id);
-				$this->fileLogger->info("User login: '{$username}' ($auth_provider)", [
-					'is_admin' => $is_admin,
-					'email' => $email,
-					'ip' => $_SERVER['REMOTE_ADDR'],
-				]);
-
-				$user = User::where('username', $username)->first();
-				if (!$user) {
-					$this->session->remove('username');
-					$this->session->remove('email');
-					$this->session->remove('is_admin');
-					$this->session->remove('auth_provider');
-					$db_user_not_found = true;
-				// user authenticated and exists in DB
-				} else {
-					// Update First and Last Name
-					if(Helper::env_bool('LDAP_UPDATE_NAME_ON_LOGIN')) {
-						User::where('username', $username)
-								->update([
-									'firstname' => $auth->getUserFirstName(),
-									'lastname' => $auth->getUserlastName(),
-								]);
-					}
-					// Load user aliases and set them in session
-					$aliases = $this->getMailAliases($user);
-
-					$this->session->set('user_aliases', $aliases);
-
-					$this->setSessionVars($this->session);
-
-					// redirect to initial requested page if exists
-					$this->initUrls();
-
-					if (!empty($login_redirect = $this->session->get('login_redirect'))) {
-						if ($login_redirect !== $this->url(RouteName::LOGIN) and
-						    $login_redirect !== $this->url(RouteName::LOGOUT) and
-						    $login_redirect !== $this->url(RouteName::ADMIN_HOMEPAGE) and
-						    $login_redirect !== $this->url(RouteName::HOMEPAGE) and
-							 $login_redirect !== $this->homepageUrl) {
-								if (str_starts_with($login_redirect, '/') && !str_starts_with($login_redirect, '//')) {
-									$url = $login_redirect;
-								} else {
-									$url = $this->homepageUrl;
-								}
-								$this->session->remove('login_redirect');
-						} else {
-							$url = $this->homepageUrl;
-						}
-					} else {
-						$url = $this->homepageUrl;
-					}
-
-					return new RedirectResponse($url);
-				}
-			}
-			// authenticate() failed
-			// db user not found after auth
-			if ($db_user_not_found) {
-				$this->fileLogger->warning("Authenticated user '$username' not found in DB.");
-				$this->flashbag->add('error', "Authentication problem. Contact admin");
-				$statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
-			} else {
+			if (!$auth->authenticate($username, $password)) {
 				$auth_provider = $auth->getAuthProvider();
 				$client_ip = $_SERVER['REMOTE_ADDR'];
 				$this->fileLogger->warning("($auth_provider) Login failed for user: '{$username}' via IP:{$client_ip}");
 				sleep((int)$_ENV['FAILED_LOGIN_TIMEOUT']);
 				$this->flashbag->add('error', "Wrong username or password");
 				$statusCode = Response::HTTP_UNAUTHORIZED;
+			} else {
+				// user is authenticated
+
+				// User does not exist is DB after auth
+				if (!$this->updateCreateUser($auth)) {
+					$username = $auth->getAuthenticatedUser();
+					$this->fileLogger->error("Authenticated user '$username' not found in DB after auth");
+					$this->flashbag->add('error', "Authentication problem. Contact admin");
+					return new RedirectResponse($this->loginUrl);
+				}
+
+				if (!empty($login_redirect = $this->session->get('login_redirect'))) {
+					$url = $this->getRedirectUrl($login_redirect);
+				} else {
+					$url = $this->homepageUrl;
+				}
+
+				return new RedirectResponse($url);
 			}
 		}
 
@@ -320,21 +236,20 @@ class LoginController extends ViewController
 			'last_login' => date("Y-m-d H:i:s")
 		];
 
+		$user = User::where('username', $username)->first();
+
 		// EXTERNAL AUTH (LDAP/OPENIDC)
-		if ($auth_provider !== "DB") {
-			$user = User::where('username', $username)->first();
-			// EXTERNAL user does not exist in DB, create him
-			if (!$user) {
-				$user = new User();
-				$user->username = $username;
-				$user->email = $email;
-				$user->firstname = $auth->getUserFirstName();
-				$user->lastname = $auth->getUserLastName();
-				$user->is_admin = $is_admin;
-				$user->auth_provider = $auth_provider_id;
-				$user->password = "EXTERNAL_AUTH";
-				$user->save();
-			}
+
+		// EXTERNAL user does not exist in DB, create him
+		if (($auth_provider !== "DB") && !$user) {
+			$user = new User();
+			$user->username = $username;
+			$user->email = $email;
+			$user->firstname = $auth->getUserFirstName();
+			$user->lastname = $auth->getUserLastName();
+			$user->is_admin = $is_admin;
+			$user->auth_provider = $auth_provider_id;
+			$user->password = "EXTERNAL_AUTH";
 		}
 		$user_id = $user->id;
 
