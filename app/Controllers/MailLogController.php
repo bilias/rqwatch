23959ad5f76d92e8001042db64f4ed2b2b53c9bf
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-07-31 18:20:50 +0300

    mention that we use with recipients

--- app/Controllers/MailLogController.php
+++ app/Controllers/MailLogController.php
@@ -428,7 +428,7 @@ class MailLogController extends ViewController
 		if ($ar['log']->mail_stored && !empty($ar['log']->mail_location)) {
 			$form_data = array(
 				'id' => $ar['log']->id,
-				'email' => $ar['log']->rcpt_to,
+				'email' => $ar['log']->rcpt_to, // with recipients
 			);
 
 			// mailreleaseform submit goes to seperate method releaseMail()
@@ -499,7 +499,7 @@ class MailLogController extends ViewController
 
 		$form_data = array(
 			'id' => $maillog->id,
-			'email' => $maillog->rcpt_to,
+			'email' => $maillog->rcpt_to, // with recipients
 		);
 
 		$mailreleaseform = MailReleaseForm::create($this->formFactory, $this->request, $form_data);
@@ -526,6 +526,7 @@ class MailLogController extends ViewController
 			// get all emails from user (primary + aliases)
 			$emails = array_unique(array_filter(array_map('strtolower', array_merge([$this->getEmail()], $this->user_aliases ?? []))));
 			// split DB recipients into array
+			// rcpt_to is with recipients
 			$rcptToList = array_map(fn($e) => strtolower(trim($e)), explode(',', $maillog->rcpt_to));
 			// split form's original recipients
 			$formEmailList = array_map(fn($e) => strtolower(trim($e)), explode(',', $data['email']));
