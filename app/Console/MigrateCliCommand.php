d180da15ad5146eb4b4c8e96833bb98deb053d5b
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-08-01 14:47:21 +0300

    Create MigrateCliCommand class to extend for migrations

---
+++ app/Console/MigrateCliCommand.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/*
+ Rqwatch
+ Copyright (C) 2025 Giannis Kapetanakis
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+namespace App\Console;
+
+use App\Core\App;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class MigrateCliCommand extends RqwatchCliCommand
+{
+	protected Capsule $capsule;
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->capsule = App::capsule();
+	}
+
+}
