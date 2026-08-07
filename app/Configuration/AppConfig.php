d6f8207d1829b9247dff08b8ec60ab5df37d3517
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-08-07 13:26:04 +0300

    Bump dev version

--- app/Configuration/AppConfig.php
+++ app/Configuration/AppConfig.php
@@ -16,7 +16,7 @@ define('RQWATCH_ROOT', realpath(__DIR__ . '/../..'));
 class AppConfig {
 
 // Application version
-public const string VERSION = '1.8.3';
+public const string VERSION = '1.8.3-dev';
 
 public const string APP_NAME = 'Rqwatch';
 
