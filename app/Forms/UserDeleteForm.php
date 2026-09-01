41735d5fa776b1eabc332415ddb09642f061a10c
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-09-01 13:51:16 +0300

    Un-used

--- app/Forms/UserDeleteForm.php
+++ app/Forms/UserDeleteForm.php
@@ -20,9 +20,7 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
-use App\Core\Routing\RouteName;
 use App\Utils\FormHelper;
 
 class UserDeleteForm extends AbstractType
