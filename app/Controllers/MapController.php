bac928075eade4a3af24a12a4ca43f3b4ca4d12e
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-09-01 15:33:07 +0300

    new lines

--- app/Controllers/MapController.php
+++ app/Controllers/MapController.php
@@ -1018,6 +1018,7 @@ class MapController extends ViewController
 		if (!is_null($id) and is_int($id)) {
 
 			$config = MapInventory::getAvailableMapConfigs($this->getRole(), $map);
+
 			if (empty($config) || empty($config['model'])) {
 				$this->fileLogger->warning("User {$this->username} tried to del all entries in " . $this->request->getPathInfo() . " for an invalid map or without authorization");
 				$this->flashbag->add('error', 'Invalid map selected');
@@ -1303,6 +1304,7 @@ class MapController extends ViewController
 		if (!is_null($id) and is_int($id)) {
 
 			$config = MapInventory::getAvailableMapConfigs($this->getRole(), $map);
+
 			if (empty($config) || empty($config['model'])) {
 				$this->fileLogger->warning("User {$this->username} tried to toggle entry in " . $this->request->getPathInfo() . " for an invalid map or without authorization");
 				$this->flashbag->add('error', 'Invalid map selected');
