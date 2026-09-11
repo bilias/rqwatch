bc2fa4cc0ed3302985af0d10a0769a41f3b24a62
Giannis Kapetanakis <bilias@edu.physics.uoc.gr>
2026-09-11 23:37:42 +0300

    Bump up version to alpha

--- app/Configuration/AppConfig.php
+++ app/Configuration/AppConfig.php
@@ -18,7 +18,7 @@ define('RQWATCH_ROOT', realpath(__DIR__ . '/../..'));
 class AppConfig {
 
 // Application version
-public const string VERSION = '2.0.9-dev';
+public const string VERSION = '2.1.0-alpha';
 
 public const string APP_NAME = 'Rqwatch';
 
